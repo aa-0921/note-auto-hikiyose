1f673be903215effe201cca1462385c034d3536d
aoyama-921 <akutoaoyama@gmail.com>
2026-01-13 09:33:29 +0900

    フォロー対象のキーワード追加

--- scripts/followFromUserSearch.js
+++ scripts/followFromUserSearch.js
@@ -6,14 +6,26 @@ import { runWithCore } from '@aa-0921/note-auto-core';
 (async () => {
   await runWithCore(async ({ core, wantsBackground }) => {
     // アカウント固有の検索ワード配列（ここで管理）
-    const searchWords = [
+    const baseSearchWords = [
       'フォロバ',
       'フォロバ100',
+      '相互',
+      'フォローバック',
     ];
+
+    const workTroubleSearchWords = [
+    ];
+
+    const otherSearchWords = [
+    ];
+
+    // すべてのリストを結合
+    const searchWords = [...baseSearchWords, ...workTroubleSearchWords, ...otherSearchWords];
 
     await core.runFollowFromUserSearch({
       background: wantsBackground,
       maxFollows: 30,
+      // コア側で options.searchWords を優先使用
       searchWords,
     });
     console.log('フォロー処理が完了しました');
